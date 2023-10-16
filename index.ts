470f56019c6434524e1b0dc548748e11b6b3b216
nihalmohd <nihalmohd828@gmail.com>
2023-10-16 20:38:05 +0530

    basic route setup is over

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 import express from 'express';
 import * as dotenv from "dotenv";
+import cors from "cors" 
+
 
 dotenv.config();
+const port = 3000;
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(cors({
+    origin: 'localhost:3000',
+    methods: ["GET", "POST"],
+  }));
+
+  app.use("/", )
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
