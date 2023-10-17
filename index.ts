09290869a596e211dbd85341a4a968c6749e0425
nihalmohd <nihalmohd828@gmail.com>
2023-10-17 11:11:46 +0530

    database data is checking and added successfuly

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as dotenv from "dotenv";
 import cors from "cors";  
 import { UserRouter } from './src/Interface/Routes/Userrouter';
+import { Database } from './src/infra/Database/config';
 
 dotenv.config();
 const port = 3000;
@@ -10,13 +11,15 @@ const app = express();
 app.use(express.json());
 
 // app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-app.use(cors({
+  //   res.send('Hello World!');
+  // });
+  
+  app.use(cors({
     origin: process.env.BASEURL, 
     methods: ["GET", "POST"],
-}));
+  }));
+  
+  Database()
 
 app.use("/", UserRouter);
 
--- src/infra/Database/config.ts
+++ src/infra/Database/config.ts
@@ -1,15 +1,15 @@
 import mssql from "mssql/msnodesqlv8"
 
-export const Database = ()=>{
-    const sqlconfig ={
-      user : "",
-      password:"",
-      server:"",
-      database:""   
+export const Database = () => {
+    const sqlconfig = {
+        server: process.env.SQLSERVER as string,
+        database: process.env.DATABASENAME as string,
+        user: process.env.USERNAME as string,
+        password: process.env.PASSWORD as string
     }
-        mssql.connect(sqlconfig).then(()=>{
-            console.log("Database connected successfully")
-        }).catch((error)=>{
-            console.log(error.message)
-        })
+    mssql.connect(sqlconfig).then(() => {
+        console.log("Database connected successfully")
+    }).catch((error) => {
+        console.log(error.message)
+    })
 }
