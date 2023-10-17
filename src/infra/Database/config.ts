d4adc043490a669920cc09aa229a543acafe9834
nihalmohd <nihalmohd828@gmail.com>
2023-10-17 15:27:15 +0530

    is over connected ms sql

--- src/infra/Database/config.ts
+++ src/infra/Database/config.ts
@@ -5,7 +5,10 @@ export const Database = () => {
         server: process.env.SQLSERVER as string,
         database: process.env.DATABASENAME as string,
         user: process.env.USERNAME as string,
-        password: process.env.PASSWORD as string
+        password: process.env.PASSWORD as string,
+        options: {
+            trustedConnection: true
+        }
     }
     mssql.connect(sqlconfig).then(() => {
         console.log("Database connected successfully")
